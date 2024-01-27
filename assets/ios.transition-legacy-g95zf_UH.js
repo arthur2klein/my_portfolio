c97e72336523fce5716e4df01f1f8fd80abaf213
Arthur Klein <kleinarthu@cy-tech.fr>
2024-01-27 13:37:25 +0100

    Updates

--- assets/ios.transition-legacy-g95zf_UH.js
+++ assets/ios.transition-legacy-g95zf_UH.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Qvlju1TA.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.o}],execute:function(){
+System.register(["./index-legacy-xwtIkaR-.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/keyboard2-legacy-LbI8MJSB.js
+++ assets/keyboard2-legacy-LbI8MJSB.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Qvlju1TA.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-xwtIkaR-.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
