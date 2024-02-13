b6940d3b4a80c256eaa7cd4fb5ad21c4c69e9604
Arthur Klein <kleinarthu@cy-tech.fr>
2024-02-13 16:28:20 +0100

    Updates

--- assets/ios.transition-legacy-zQFLyVWq.js
+++ assets/ios.transition-legacy-zQFLyVWq.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-hsIhmdZS.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.o}],execute:function(){
+System.register(["./index-legacy-FoW84cKK.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
