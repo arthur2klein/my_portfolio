dd50f3fad580258e3065d6b885381371c9039a66
Arthur Klein <kleinarthu@cy-tech.fr>
2024-02-21 15:47:58 +0100

    First version of TabMoi

--- src/pages/TabAccueil.tsx
+++ src/pages/TabAccueil.tsx
@@ -19,22 +19,22 @@ const TabAccueil: React.FC = () => {
         <IonGrid class="fill-space column">
           <IonRow class="three-per-row">
             <IonCol class="icon icon-moi">
-              <IonButton className="fill-space" routerLink="/my_portfolio/tab-moi">Moi</IonButton>
+              <IonButton className="fill-space button-acc" routerLink="/my_portfolio/tab-moi">Moi</IonButton>
             </IonCol>
             <IonCol class="icon icon-comp">
-              <IonButton className="fill-space" routerLink="/my_portfolio/tab-comp">Mes Compétences</IonButton>
+              <IonButton className="fill-space button-acc" routerLink="/my_portfolio/tab-comp">Mes Compétences</IonButton>
             </IonCol>
             <IonCol class="icon icon-expe">
-              <IonButton className="fill-space" routerLink="/my_portfolio/tab-expe">Mon Expérience</IonButton>
+              <IonButton className="fill-space button-acc" routerLink="/my_portfolio/tab-expe">Mon Expérience</IonButton>
             </IonCol>
             <IonCol class="icon icon-proj">
-              <IonButton className="fill-space" routerLink="/my_portfolio/tab-proj">Mes Projets</IonButton>
+              <IonButton className="fill-space button-acc" routerLink="/my_portfolio/tab-proj">Mes Projets</IonButton>
             </IonCol>
             <IonCol class="icon icon-pass">
-              <IonButton className="fill-space" routerLink="/my_portfolio/tab-pass">Mes Passions</IonButton>
+              <IonButton className="fill-space button-acc" routerLink="/my_portfolio/tab-pass">Mes Passions</IonButton>
             </IonCol>
             <IonCol class="icon icon-cont">
-              <IonButton className="fill-space" routerLink="/my_portfolio/tab-cont">Me Contacter</IonButton>
+              <IonButton className="fill-space button-acc" routerLink="/my_portfolio/tab-cont">Me Contacter</IonButton>
             </IonCol>
           </IonRow>
         </IonGrid>
--- src/pages/TabMoi.tsx
+++ src/pages/TabMoi.tsx
@@ -16,8 +16,51 @@ const TabMoi: React.FC = () => {
             <IonTitle size="large">Moi</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <div className="placeholder">
-          <h1>Cette page me présentera briévement à travers mon expérience et mes objectifs.</h1>
+        <div className="moi">
+          <div className="name-moi">
+            KLEIN Arthur
+          </div>
+          <div className="line-moi">
+            <img className="photo" src="arthur20230627.jpg" />
+
+            <div className="cadre-alt desc-moi">
+              Je suis <em>KLEIN Arthur</em>, passionné d'informatique depuis
+              une formation débutant au lycée (filière S Abibac) à travers les
+              cours d'Informatique et Création Numérique, et en CPGE (MP*) à
+              travers les cours générals et de spécialité Informatique. Cet
+              éveil m'a mené à terminer ma fomation à <em>CY-Tech</em> où mon
+              intért s'est particulièrement développé pour la création
+              de <em>frontends</em>, explorant notamment React et Flutter,
+              d'<em>architecture d'application</em> utilisant divers
+              technologies de Backend ainsi que github actions et Kubernetes,
+              et dans l'<em>analyse de données</em> et l'IA avec notamment
+              PySpark et Tensorflow.
+            </div>
+
+            <div className="cadre expe-moi">
+              L'<em>architecture de projets</em>, l'utilisation de Git, Docker,
+              Kubernetes, et d'autres outils ont été des piliers de ma
+              spécialisation en Cloud Computing à CY-Tech. Je consolide cette
+              expertise avec la maîtrise du développement d'interfaces pour
+              le <em>web</em> et le <em>mobile</em>, tout en approfondissant
+              mes compétences à travers des cours de traitement de données et
+              des projets inter-options intégrant l'<em>Intelligence
+              Artificielle</em> pour une expérience utilisateur enrichissante.
+            </div>
+
+            <div className="cadre obje-moi">
+              À la suite de ma formation, je suis à la recherche d'un <em>stage
+              de fin d'étude</em>. Mon objectif est de m'investir pleinement
+              dans des projets stimulants qui mettent en lumière mes
+              compétences.  Passionné par l'apprentissage continu, je souhaite
+              exploiter cette opportunité pour <em>approfondir et diversifier
+              mes connaissances</em>. Contribuer à des projets innovants où
+              mon expertise sera valorisée est ma priorité. Si vous recherchez
+              un stagiaire dynamique, curieux et prêt à relever de nouveaux
+              défis, n'hésitez pas à me contacter.
+            </div>
+
+          </div>
         </div>
       </IonContent>
     </IonPage>
