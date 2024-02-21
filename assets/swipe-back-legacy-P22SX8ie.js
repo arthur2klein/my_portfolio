44e81d1862c83f28b800aa4ee7401b0b7f399203
Arthur Klein <kleinarthu@cy-tech.fr>
2024-02-21 22:01:26 +0100

    Updates

--- assets/swipe-back-legacy-P22SX8ie.js
+++ assets/swipe-back-legacy-P22SX8ie.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-lSZcNMbJ.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-dmXZMhOF.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
