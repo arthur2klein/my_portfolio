3f1bf39614378a59d526afcb213a3e2551bec489
Arthur Klein <kleinarthu@cy-tech.fr>
2024-02-21 15:57:33 +0100

    Updates

--- assets/keyboard2-legacy-iAx_NKe1.js
+++ assets/keyboard2-legacy-iAx_NKe1.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-h5vsg445.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-lSZcNMbJ.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
