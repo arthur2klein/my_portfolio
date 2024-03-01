80aebdf0db4db974a9f35092331db48479ec5faa
Arthur Klein <kleinarthu@cy-tech.fr>
2024-03-01 16:46:19 +0100

    Updates

--- assets/ios.transition-legacy-z5RB5Zmr.js
+++ assets/ios.transition-legacy-z5RB5Zmr.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-5s2qpjeQ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-yjox9YPj.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
