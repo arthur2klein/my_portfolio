ff805bb244c288e11f063a3c70abeeea2ac859c1
Arthur Klein <kleinarthu@cy-tech.fr>
2024-03-11 09:24:42 +0100

    Updates

--- assets/keyboard2-legacy-rH4ERuFR.js
+++ assets/keyboard2-legacy-rH4ERuFR.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-MC4jsWnG.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-Teo53pgN.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
