501ce2444555eed0ab1e3a7f9fd004a8097e57a1
Arthur Klein <kleinarthu@cy-tech.fr>
2024-03-11 09:29:28 +0100

    Updates

--- assets/swipe-back-legacy-D9y3DM6u.js
+++ assets/swipe-back-legacy-D9y3DM6u.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Teo53pgN.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-bde0TJPU.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
