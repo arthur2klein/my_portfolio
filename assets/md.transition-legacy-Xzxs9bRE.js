ac524e59ccac18eeb873d895369b4ffac8122463
Arthur Klein <kleinarthu@cy-tech.fr>
2024-07-07 17:10:25 +0200

    Updates

--- assets/md.transition-legacy-Xzxs9bRE.js
+++ assets/md.transition-legacy-Xzxs9bRE.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Kpty39t4.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-wTMq4cni.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
