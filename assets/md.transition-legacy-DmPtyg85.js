e92efdccc6a3128d5c4de12379a2fe62f4472ecf
aklein <arthur2kleingm@gmail.com>
2025-04-25 18:54:46 +0200

    Updates

--- assets/md.transition-legacy-DmPtyg85.js
+++ assets/md.transition-legacy-DmPtyg85.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-wTMq4cni.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-BEEqm2Hn.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
